62e9222efe8cc92512c330c0a34b1a30983c2a9a
KAUA PEREIRA <54439995837@info.local>
2024-05-13 17:18:37 -0300

    js gamifica

---
+++ img/Nova pasta (2)/ex2.js
@@ -0,0 +1,34 @@
+//Exercício 2: Verificação de dia da semana
+//Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da
+//semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira,
+//2 representa terça-feira e assim por diante.
+
+let day = prompt("escreva um número de 1 a 7:");
+
+if (day == "1") {
+    console.log ("Hoje é Domingo!")
+}
+
+if (day == "2") {
+    console.log ("Hoje é Segunda-Feira!");
+}
+
+if (day == "3") {
+    console.log ("Hoje é Terça-Feira!");
+}
+
+if (day == "4") {
+    console.log ("Hoje é Quarta-Feira!");
+}
+
+if (day == "5") {
+    console.log ("Hoje é Quinta-Feira!!");
+}
+
+if (day == "6") {
+    console.log ("Hoje é Sexta-Feira!");
+}
+
+if (day == "7") {
+    console.log ("Hoje é Sábado!");
+}
