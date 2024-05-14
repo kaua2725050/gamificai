9638e617bb6e9d69471ec228f6c0c4fe06886fd7
KAUA PEREIRA <54439995837@info.local>
2024-05-14 17:22:34 -0300

    js extra

--- script.js
+++ script.js
@@ -26,3 +26,42 @@ window.onresize = () => {
     iconeX.style.display = "inline"
     iconeBarras.style.display = "none"
 }
+
+
+
+let slides = [
+    'primeiro-banner',
+    'segundo-banner',
+    'terceiro-banner'
+]
+
+let slideAtual = 0
+
+let numeroSlides = slides.length
+
+
+let banner = document.querySelectorAll(".banner")
+
+banner.classList.add(slides[slideAtual])
+
+const mostrarProximoSlide = () => {
+    banner.classList.remove(slides[slideAtual])
+
+if (slideAtual < (numeroSlides - 1) ) {
+    slideAtual++
+} else {
+    slideAtual = 0
+}
+
+    banner.classList.add(slides[slideAtual])
+}
+
+const mostrarSlideAnterior = () => {
+    banner.classList.remove(slides[slideAtual])
+
+    if (slideAtual > 0) {
+        slideAtual--
+    } else {
+        slideAtual = numeroSlides - 1
+    }
+}
